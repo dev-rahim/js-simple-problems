5aed55853692c6dcbe73615a34d6bb3943716888
dev-rahim <rahimuddin5915@gmail.com>
2022-02-13 21:28:47 +0600

    using Math function

---
+++ math.js
@@ -0,0 +1,32 @@
+// math abs 
+/*
+var nagetiveNumber = -12;
+var absulateNagetiveNumber = Math.abs( nagetiveNumber );
+console.log( absulateNagetiveNumber );
+*/
+// math ceil
+/* 
+const flotingNumber = 5.52;
+let ceilIntNumber = Math.ceil( flotingNumber );
+console.log( ceilIntNumber );
+*/
+// Math floor
+/*
+const flotingNumber = 5.99;
+let floorIntNumber = Math.floor( flotingNumber );
+console.log(floorIntNumber);
+*/
+// Math.round
+/*
+const flotingNumber = 5.50;
+let roundIntNumber = Math.round( flotingNumber );
+console.log( roundIntNumber );
+*/
+// Math.random;
+/*
+for ( let i = 1; i < 500; i++ ) {
+    let randomNumber = Math.random() * 6;
+    let intrandomNumber = Math.ceil( randomNumber );
+    console.log( intrandomNumber );
+}
+*/
